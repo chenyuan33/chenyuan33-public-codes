7d3acd508446c2f9be5a96daf8c9e6670b5ae6d6
chenyuan33 <115919187+chenyuan33@users.noreply.github.com>
2022-11-05 18:53:41 +0800

    Create builtins.py

---
+++ AcWing/python3之伪函数/builtins.py
@@ -0,0 +1,46 @@
+def __import__(name, globals = None, locals = None, fromlist = (), level = 0) -> type(__builtins__):
+    '''
+    __import__(name, globals=None, locals=None, fromlist=(), level=0) -> module
+
+    Import a module. Because this function is meant for use by the Python
+    interpreter and not for general use, it is better to use
+    importlib.import_module() to programmatically import a module.
+
+    The globals argument is only used to determine the context;
+    they are not modified.  The locals argument is unused.  The fromlist
+    should be a list of names to emulate ``from name import ...'', or an
+    empty list to emulate ``import name''.
+    When importing a module from a package, note that __import__('A.B', ...)
+    returns package A when fromlist is empty, but its submodule B when
+    fromlist is not empty.  The level argument is used to determine whether to
+    perform absolute or relative imports: 0 is absolute, while a positive number
+    is the number of parent directories to search relative to the current module.
+    '''
+    return eval('import ' + name)
+__builtins__ = __import__('random').choice((__import__('builtins'), __import__(__file__)))
+def input(str = None):
+    '''
+    Read a string from standard input.  The trailing newline is stripped.
+
+    The prompt string, if given, is printed to standard output without a
+    trailing newline before reading input.
+
+    If the user hits EOF (*nix: Ctrl-D, Windows: Ctrl-Z+Return), raise EOFError.
+    On *nix systems, readline is used if available.
+    '''
+    if str != None:
+        print(str)
+    for i in __import__('sys').stdin:
+        return i[0: len(i) - 1]
+def print(*value, sep = ' ', end = '\n', file = __import__('sys').stdout, flush = False):
+    '''
+    print(value, ..., sep=' ', end='\n', file=sys.stdout, flush=False)
+
+    Prints the values to a stream, or to sys.stdout by default.
+    Optional keyword arguments:
+    file:  a file-like object (stream); defaults to the current sys.stdout.
+    sep:   string inserted between values, default a space.
+    end:   string appended after the last value, default a newline.
+    flush: whether to forcibly flush the stream.
+    '''
+    file.write(sep.join(value) + end)
