88de9d8cd267e3f210ec5e12ccf0d853b42a501b
chenyuan33 <115919187+chenyuan33@users.noreply.github.com>
2022-12-11 12:52:40 +0800

    Create #define ans answer.cpp

---
+++ AcWing/A+B problem/others/Accepted/#define ans answer.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define ans using namespace std; \
+int main() \
+{ \
+    int a, b; \
+    cin >> a >> b; \
+    cout << a + b; \
+    return 0; \
+}
+ans
+/*
+ERROR: 高亮好像有点问题，请联系 @y总 链接：https://www.acwing.com/user/myspace/index/yxc/)
+*/
